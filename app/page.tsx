fde45529b1ef6cfb3f54dd2649b8f5ccef021e37
Rahul Choudhury <rchoudhury63@gmail.com>
2025-05-13 11:25:36 +0530

    feat: dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { BLOCKS } from "@/markdown";
 
 export default function Home() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
-      <header className="flex flex-col gap-1">
-        <h1 className="text-3xl font-bold tracking-tight">Custom Registry</h1>
-        <p className="text-muted-foreground">
-          A custom registry for distributing code using shadcn.
-        </p>
-      </header>
+      <div className="flex justify-between">
+        <header className="flex flex-col gap-1">
+          <h1 className="text-3xl font-bold tracking-tight">Custom Registry</h1>
+          <p className="text-muted-foreground">
+            A custom registry for distributing code using shadcn.
+          </p>
+        </header>
+        <ModeToggle />
+      </div>
       <main className="flex flex-col flex-1 gap-8">
         {BLOCKS.map((Block, idx) => (
           <section className="border rounded-lg p-6" key={idx}>
