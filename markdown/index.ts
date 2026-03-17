ffe673dd6903a9a9257042ffac28b2aacb2f6861
Rahul Choudhury <rchoudhury63@gmail.com>
2026-03-17 18:19:35 +0530

    refactor(registry): remove use-carousel-scale hook

--- markdown/index.ts
+++ markdown/index.ts
@@ -4,14 +4,12 @@ import MultiStepForm from "./multi-step-form.mdx";
 import SubmitButton from "./submit-button.mdx";
 import TailwindHelper from "./tailwind-helper.mdx";
 import UseCarouselApi from "./use-carousel-api.mdx";
-import UseCarouselScale from "./use-carousel-scale.mdx";
 import UseMediaQuery from "./use-media-query.mdx";
 
 const blocks = [
   { Component: MetaPixel, name: "meta-pixel" },
   { Component: TailwindHelper, name: "tailwind-helper" },
   { Component: UseCarouselApi, name: "use-carousel-api" },
-  { Component: UseCarouselScale, name: "use-carousel-scale" },
   { Component: MultiStepForm, name: "multi-step-form" },
   { Component: SubmitButton, name: "submit-button" },
   { Component: UseMediaQuery, name: "use-media-query" },
